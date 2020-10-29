e226b70e73ebd69959a312f43c504a084625269a
Denis Malyshev <malyshev_denis@outlook.com>
2020-10-29 22:19:17 +0400

    Реализовано задание 4. Отрисовка границ поля

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,30 @@
 export function calcTileType(index, boardSize) {
-  // TODO: write logic here
+  switch(index) {
+    case (0):
+      return 'top-left';
+    case (boardSize - 1):
+      return 'top-right';
+    case (boardSize ** 2 - 1):
+      return 'bottom-right';
+    case ((boardSize ** 2) - boardSize):
+      return 'bottom-left';
+  }
+
+  for(let i = 1; i <= boardSize - 1; i += 1){
+    if (i === index) return 'top';
+  }
+
+  for(let i = boardSize; i <= boardSize * (boardSize - 2); i += boardSize){
+    if(i === index) return 'left'
+  }
+
+  for(let i = boardSize + boardSize - 1; i <= boardSize * (boardSize - 2) + boardSize; i += boardSize){
+    if(i === index) return 'right';
+  }
+
+  for(let i = boardSize ** 2 - 2; i >= boardSize ** 2 - boardSize; i -= 1){
+    if(i === index) return 'bottom';
+  }
   return 'center';
 }
 
