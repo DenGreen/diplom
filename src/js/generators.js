f92719ffdea33f59555f1a7e70dbc94c9e259067
Denis Malyshev <malyshev_denis@outlook.com>
2020-11-03 21:30:47 +0400

    Задача 5.Team Generation(поиск нужных ячеек и отррисовка персонажей)

--- src/js/generators.js
+++ src/js/generators.js
@@ -5,9 +5,38 @@
  * @param maxLevel max character level
  * @returns Character type children (ex. Magician, Bowman, etc)
  */
-export function* characterGenerator(allowedTypes, maxLevel) {
-  // TODO: write logic here
+
+ let arr = ['bowman', 'daemon', 'swordsman', 'magician', 'undead', 'vampire']
+
+export function* characterGenerator(allowedTypes, maxLevel = 1) {
+  let element = document.getElementsByClassName('cell');
+  let arrElement = Array.from(element);
+  let max = 8;
+  let arrColumnPlayer = [];
+  let arrColumnComputer = [];
+
+  let maxCharacter = 1;
+
+  for(let i = 0; i <= max - 1; i += 1){
+    arrColumnPlayer.push(arrElement[0 + i  * max]);
+    arrColumnPlayer.push(arrElement[1 + i  * max]);
+    arrColumnComputer.push(arrElement[6 + i  * max]);
+    arrColumnComputer.push(arrElement[7 + i  * max]);
+  }
+
+  for(let i = 0; i <= maxCharacter - 1; i += 1){
+    arrColumnPlayer[getRandom(max + max)].innerHTML = `
+    <div class="character ${arr[getRandom(6)]}"></div>`;
+    arrColumnComputer[getRandom(max + max)].innerHTML = `
+    <div class="character ${arr[getRandom(6)]}"></div>`;
+  }
 }
+
+function getRandom(max) {
+  return Math.floor(Math.random() * max);
+}
+
+characterGenerator(arr, maxLevel = 1)
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
   // TODO: write logic here
