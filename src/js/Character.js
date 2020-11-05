4fcb3488fd3e5a8bbb9a2399e403f7d5470c96ab
Denis Malyshev <malyshev_denis@outlook.com>
2020-11-05 20:48:01 +0400

    Реализация задачи 6.Запрет создания объектов Character

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,5 +1,6 @@
 export default class Character {
   constructor(level, type = 'generic') {
+    if(new.target === Character) throw Error('Запрещено создавать экземпляры класса Character');
     this.level = level;
     this.attack = 0;
     this.defence = 0;
