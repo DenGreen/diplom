98eee2c56b54817288cb50277e5d5576c6f18822
Denis Malyshev <malyshev_denis@outlook.com>
2020-11-24 20:27:08 +0400

    Задание 10.2

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -5,16 +5,21 @@ export default class GameController {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
     this.indexSelect = null;
+    this.arrRadius = null;
+    this.indexTornSelect = null;
   }
 
   init() {
     const onCellEnterBind = this.onCellEnter.bind(this);
     const onCellLeaveBind = this.onCellLeave.bind(this);
     const onCellClickBind = this.onCellClick.bind(this);
+    const onTurnEnterBind = this.onTurnEnter.bind(this);
     this.gamePlay.drawUi(themes.prairie);
+    this.gamePlay.addCellEnterListener(onTurnEnterBind);
     this.gamePlay.addCellEnterListener(onCellEnterBind);
     this.gamePlay.addCellLeaveListener(onCellLeaveBind);
     this.gamePlay.addCellClickListener(onCellClickBind);
+    
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
   }
@@ -23,6 +28,12 @@ export default class GameController {
     const char = this.gamePlay.cells[index].querySelector(".character");
     if(char.classList.contains('bowman') || char.classList.contains('swordsman') || char.classList.contains('magician')){
       if (this.indexSelect !== null) this.gamePlay.deselectCell(this.indexSelect);
+
+
+      this.arrRadius = this.gamePlay.radiusTurn(index); /** Тут хранятся все ячейки на которые может пойти персонаж */
+
+      this.gamePlay.setCursor('pointer');
+
       this.indexSelect = index;
       this.gamePlay.selectCell(index);
     } else {
@@ -33,13 +44,13 @@ export default class GameController {
   onCellEnter(index) {
     if (this.gamePlay.cells[index].querySelector(".character")) {
       let pers = null;
-      
+      this.gamePlay.setCursor('pointer');
       this.gamePlay.arrChar.forEach((element) => {
         if (element.position === index) {
           pers = element.character;
         }
       });
-
+      
       this.gamePlay.showCellTooltip(
         `🎖${pers.level} ⚔${pers.attack} 🛡${pers.defence} ❤${pers.health}`,
         index
@@ -47,7 +58,23 @@ export default class GameController {
     }
   }
 
+  /** Метод использует окружение выбранного персонажа, для отрисовки разрешенных ходов */
+
+  onTurnEnter(index) {
+    if (this.arrRadius !== null) {
+        for(let arr of this.arrRadius) {
+          if(arr === index) {
+            this.gamePlay.setCursor('pointer');
+            this.gamePlay.selectCell(index, 'green');
+            this.indexTornSelect = index;
+          }
+        }
+    }
+  }
+
   onCellLeave(index) {
+    this.gamePlay.setCursor('auto')
     this.gamePlay.hideCellTooltip(index);
+    if (this.indexTornSelect !== null) this.gamePlay.deselectCell(this.indexTornSelect);
   }
 }
